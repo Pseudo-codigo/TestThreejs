31dad48cb92415e10d3b9ca97fb92aea4933a13c
Carlos Morales <carlos.morales@cit.hn>
2023-11-09 09:50:07 -0600

    Env map
    .

--- src/script.js
+++ src/script.js
@@ -1,15 +1,28 @@
-import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import { GroundProjectedSkybox } from 'three/addons/objects/GroundProjectedSkybox.js'
+
+/**
+ * Loaders
+ */
+const glftLoader = new GLTFLoader();
+
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const rgbeLoader = new RGBELoader()
+
+const textureLoader = new THREE.TextureLoader()
+
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+const global = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,73 +30,121 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// /**
-//  * Axes Helper
-//  */
-// const axesHelper = new THREE.AxesHelper()
-// scene.add(axesHelper)
-
 /**
- * Textures
+ * Update all materials
  */
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
-
-
-/**
- * Fonts
- */
-
-const fontLoader = new FontLoader()
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) => {
-        const textGeometry = new TextGeometry(
-            'Hello Three.js',
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 5,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 4
-            }
-        )
-        console.log('object :>> ', textGeometry.parameters);
-
-        textGeometry.computeBoundingBox()
-        // textGeometry.translate(
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
-        // )
-        textGeometry.center()
-
-        const material = new THREE.MeshMatcapMaterial()
-        // textMaterial.wireframe = true
-        material.matcap = matcapTexture
-        const text = new THREE.Mesh(textGeometry, material)
-        scene.add(text)
-
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-
-        for (let i = 0; i < 300; i++) {
-            const donut = new THREE.Mesh(donutGeometry, material)
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-
-            const scale = Math.random();
-            donut.scale.set(scale, scale, scale)
-
-            scene.add(donut)
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if (child.isMesh && child.material.isMeshStandardMaterial) {
+            child.material.envMapIntensity = global.envMapIntensity
         }
+    })
+}
+
+
+/**
+ * Environment map
+ */
+
+
+scene.backgroundBlurriness = 0
+scene.backgroundIntensity = 1
+
+gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001)
+gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
+
+
+// Global intensity
+global.envMapIntensity = 1
+gui.add(global, 'envMapIntensity')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .onChange(updateAllMaterials)
+
+// // LDR cube texture
+// const environmentMap = cubeTextureLoader.load([
+//     '/environmentMaps/0/px.png',
+//     '/environmentMaps/0/nx.png',
+//     '/environmentMaps/0/py.png',
+//     '/environmentMaps/0/ny.png',
+//     '/environmentMaps/0/pz.png',
+//     '/environmentMaps/0/nz.png'
+// ])
+
+// scene.environment = environmentMap
+// scene.background = environmentMap
+
+
+// rgbeLoader.load('/environmentMaps/2/2k.hdr', (environmentMap) => {
+//     environmentMap.mapping = THREE.EquirectangularReflectionMapping
+
+//     // scene.background = environmentMap
+//     scene.environment = environmentMap
+
+//     const skybox = new GroundProjectedSkybox(environmentMap)
+//     skybox.radius = 120
+//     skybox.height = 11
+
+//     gui.add(skybox, 'radius', 1, 200, 0.1).name('skyboxRadius')
+//     gui.add(skybox, 'height', 1, 100, 0.1).name('skyboxHeight')
+
+//     skybox.scale.setScalar(50)
+//     scene.add(skybox)
+// })
+
+/**
+ * Realtime environment map 
+ */
+
+// Base environment map
+const environmentMap = textureLoader.load('/environmentMaps/blockadesLabsSkybox/interior_views_cozy_wood_cabin_with_cauldron_and_p.jpg')
+environmentMap.mapping = THREE.EquirectangularReflectionMapping
+environmentMap.colorSpace = THREE.SRGBColorSpace
+
+scene.background = environmentMap
+
+// Holy donut
+const holyDonut = new THREE.Mesh(
+    new THREE.TorusGeometry(8, 0.5),
+    new THREE.MeshBasicMaterial({ color: new THREE.Color(10, 4, 2) })
+)
+holyDonut.layers.enable(1)
+holyDonut.position.y = 3.5
+scene.add(holyDonut)
+
+// Cube render target
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+    type: THREE.HalfFloatType
+})
+
+scene.environment = cubeRenderTarget.texture
+
+// Cube camera
+const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget)
+cubeCamera.layers.set(1)
+
+/**
+ * Torus Knot
+ */
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+    new THREE.MeshStandardMaterial({ roughness: 0.3, metalness: 1, color: 0xaaaaaa })
+)
+torusKnot.position.x = -4
+torusKnot.position.y = 4
+scene.add(torusKnot)
+
+/**
+ * Models
+ */
+glftLoader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(10, 10, 10)
+        scene.add(gltf.scene)
+
+        updateAllMaterials()
     }
 )
 
@@ -114,13 +175,12 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.set(14, 5, 14)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
+controls.target.y = 3.5
 controls.enableDamping = true
 
 /**
@@ -136,9 +196,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
-
 const tick = () => {
+    // Time
     const elapsedTime = clock.getElapsedTime()
+
+    // Real time environment map
+    if (holyDonut) {
+        holyDonut.rotation.x = Math.sin(elapsedTime) * 2
+
+        cubeCamera.update(renderer, scene)
+    }
 
     // Update controls
     controls.update()
